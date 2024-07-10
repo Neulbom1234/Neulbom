541e0238f07bde6d555322395ba455294f1e497e
san9901 <san990190@gmail.com>
2024-07-10 17:22:12 +0900

    post 상세 페이지(1)
    react-slick 추가

--- front/src/app/(main)/[username]/[id]/page.tsx
+++ front/src/app/(main)/[username]/[id]/page.tsx
@@ -1,7 +1,32 @@
+import Header from './_component/Header'
+import style from './postPage.module.css'
+import {faker} from '@faker-js/faker';
+import ImageSlider from './_component/ImageSlider';
 
 export default function Home() {
+  const target = {
+    postId: 1,
+    User: {
+      id: 'elonmusk',
+      nickname: 'Elon Musk',
+    },
+    content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+    createdAt: new Date(),
+    Images: [] as any[],
+  }
+
+  target.Images.push(
+    {imageId: 1, link: faker.image.urlLoremFlickr()},
+    {imageId: 2, link: faker.image.urlLoremFlickr()},
+    {imageId: 3, link: faker.image.urlLoremFlickr()},
+  )
+
     return (
-      <div>게시글 상세 페이지
+      <div className={style.main}>
+        <Header/>
+        <div className={style.imageWrapper}>
+          <ImageSlider target={target}/>
+        </div>
       </div>
     )
   }
