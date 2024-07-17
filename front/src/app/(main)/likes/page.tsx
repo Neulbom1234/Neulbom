4a4ba016eb6cc65ef622463775468f849e61d7cd
san9901 <san990190@gmail.com>
2024-07-17 13:37:16 +0900

    [style]   : close button 코드 삭제 page, css 파일에서 close button 코드 삭제

--- front/src/app/(main)/likes/page.tsx
+++ front/src/app/(main)/likes/page.tsx
@@ -33,15 +33,6 @@ export default function Page() {
     <>
       <div className={style.main}>
         <div className={style.header}>
-          <button className={style.closeButton} onClick={onClickClose}>
-              <svg width={24} viewBox="0 0 24 24" aria-hidden="true"
-                  className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-z80fyv r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-19wmn03">
-                <g>
-                  <path
-                    d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
-                </g>
-              </svg>
-            </button>
           <div className={style.like}>좋아하는 게시글</div>
         </div>
 
