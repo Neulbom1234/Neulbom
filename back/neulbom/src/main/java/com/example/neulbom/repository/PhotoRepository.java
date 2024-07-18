918fe0bcb32b3e2d5c69330f12f3f13e3d4d18d8
DongJun Park <121371412+qkrehdwns032@users.noreply.github.com>
2024-07-18 13:14:04 +0900

    [feat] gender 게시글 찾기 추가

--- back/neulbom/src/main/java/com/example/neulbom/repository/PhotoRepository.java
+++ back/neulbom/src/main/java/com/example/neulbom/repository/PhotoRepository.java
@@ -4,6 +4,7 @@ import com.example.neulbom.domain.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,5 @@ public interface PhotoRepository extends JpaRepository<Photo, Long> {
 
     Optional<Photo> findById(long id);
 
+    List<Photo> findByGender(String gender);
 }
