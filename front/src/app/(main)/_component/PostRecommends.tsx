5bd0e9a8fe3e5d0957e301b0cd9da59adc6db213
san9901 <san990190@gmail.com>
2024-07-27 23:18:46 +0900

    [feat] loading창 추가하기
    데이터를 가져오는 컴포넌트에 loading창 추가

--- front/src/app/(main)/_component/PostRecommends.tsx
+++ front/src/app/(main)/_component/PostRecommends.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { getPostRecommends } from "../_lib/getPostRecommends";
 import Post from "./Post";
 import type { Post as IPost } from "@/model/Post";
 
 export default function PostRecommends() {
-  const { data } = useQuery<IPost[]>({
+  const { data } = useSuspenseQuery<IPost[]>({ //인피니트 스크롤링 할 때 useInfiniteQuery말고 useSuspenseInfiniteQuery 쓰기! 이유는 섹션3 마지막 강의 참조하셈
     queryKey: ['posts', 'recommends'], 
     queryFn: getPostRecommends,
     staleTime: 60 * 1000, //fresh -> stale로 바뀌는 시간, gcTime보다 작아야함
--- front/src/app/(main)/page.tsx
+++ front/src/app/(main)/page.tsx
@@ -1,32 +1,22 @@
 import style from "@/app/(main)/home.module.css"
-import Post from "./_component/Post"
 import CommonLayout from "./_component/CommonLayout"
 import Header from "./_component/Headet";
-import { HydrationBoundary, QueryClient, dehydrate } from "@tanstack/react-query";
-import { getPostRecommends } from "./_lib/getPostRecommends";
-import PostRecommends from "./_component/PostRecommends";
 import CategoryProvider from "./_component/CategoryProvider";
-import CategoryDecider from "./_component/CategoryDecider";
+import { Suspense } from "react";
+import Loading from "./loading";
+import CategoryDeciderSuspense from "./_component/CategoryDeciderSuspense";
 
 export default async function Home() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ['posts', 'recommends'],
-    queryFn: getPostRecommends
-  })
-  const dehydratedState = dehydrate(queryClient) //hydrate는 서버에서 온 데이터를 클라이언트에서 그대로 물려받는 것
 
   return (
-    <HydrationBoundary state={dehydratedState}>
-      <div className={style.main}>
-        <CategoryProvider>
-          <Header/>
-          <div className={style.postsWrapper}>
-            <CategoryDecider/>
-          </div>
-          <CommonLayout/>
-        </CategoryProvider>
-      </div>
-    </HydrationBoundary>
+    <div className={style.main}>
+      <CategoryProvider>
+        <Header/>
+        <Suspense fallback={<Loading/>}>
+          <CategoryDeciderSuspense/>
+        </Suspense>
+        <CommonLayout/>
+      </CategoryProvider>
+    </div>
   )
 }
