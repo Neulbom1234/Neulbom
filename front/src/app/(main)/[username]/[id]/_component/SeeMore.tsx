d086c39263b33265d5a78b16d9b59f5878b5194e
san9901 <san990190@gmail.com>
2024-09-13 16:46:36 +0900

    [feat] 게시글 삭제하기 추가(1)
    alert dialog 추가
    api 연결, 디자인 수정 추후 예정

--- front/src/app/(main)/[username]/[id]/_component/SeeMore.tsx
+++ front/src/app/(main)/[username]/[id]/_component/SeeMore.tsx
@@ -1,18 +1,15 @@
-"use client"
-
-import React, { useEffect, useState } from 'react';
-import style from './seeMore.module.css';
+import React, { useState } from 'react';
+import { AlertDialog, AlertDialogTrigger, AlertDialogContent, AlertDialogAction, AlertDialogCancel } from '@radix-ui/react-alert-dialog';
 import { MoreOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
 import { Dropdown, Space } from 'antd';
-import { signOut, useSession } from 'next-auth/react';
 import { User } from '@/model/User';
+import style from './seeMore.module.css';
 
 type Props = {
   writeUser: User
 }
 
-const items: MenuProps['items'] = [
+const items = [
   {
     label: "클립보드 복사",
     key: '0',
@@ -23,33 +20,59 @@ const items: MenuProps['items'] = [
   },
 ];
 
-const loginItems: MenuProps['items'] = [
-  {
-    label: "클립보드 복사",
-    key: '0',
-  },
-  {
-    label: "삭제하기",
-    key: '1',
-  },
-];
+export default function SeeMore({ writeUser }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
 
-export default function SeeMore({writeUser}: Props) {
-  const { data: session, status } = useSession();
-
-  useEffect(() => {
-    console.log('Session data:', JSON.stringify(session, null, 2));
-  }, [session]);
+  const loginItems = [
+    {
+      label: "클립보드 복사",
+      key: '0',
+    },
+    {
+      label: (
+        <span
+          onClick={() => setIsOpen(true)} // Open dialog on click
+        >
+          삭제하기
+        </span>
+      ),
+      key: '1',
+    },
+  ];
 
   return (
     <>
-      <Dropdown menu={ {items: writeUser.name === session?.user?.name ? loginItems : items}} trigger={['click']}>
+      <Dropdown menu={{ items: writeUser.name ? loginItems : items }} trigger={['click']}>
         <a onClick={(e) => e.preventDefault()}>
           <Space>
-            <MoreOutlined className={style.icon}/>
+            <MoreOutlined />
           </Space>
         </a>
       </Dropdown>
+
+      {/* Alert Dialog */}
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialogContent className={style['alert-dialog-content']}>
+          <div>
+            <h2>삭제하시겠습니까?</h2>
+            <p>이 작업은 되돌릴 수 없습니다. 게시글이 영구적으로 삭제됩니다.</p>
+          </div>
+          <div>
+            <AlertDialogCancel asChild>
+              <button className="btn-cancel" onClick={() => setIsOpen(false)}>취소</button>
+            </AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <button className="btn-delete" onClick={() => {
+                // 삭제 로직 처리
+                setIsOpen(false);
+              }}>삭제</button>
+            </AlertDialogAction>
+          </div>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      {/* 오버레이 */}
+      {isOpen && <div className={style['alert-dialog-overlay']} onClick={() => setIsOpen(false)} />}
     </>
-  )
+  );
 }
