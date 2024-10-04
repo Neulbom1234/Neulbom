9a94f2112358705f884db71d809dd90b4a43f7ee
san9901 <san990190@gmail.com>
2024-10-04 16:10:43 +0900

    [feat] myPage 프로필 구현

--- front/src/app/(main)/_component/NavMenu.tsx
+++ front/src/app/(main)/_component/NavMenu.tsx
@@ -15,7 +15,7 @@ export default function NavMenu() {
       <li> {/* 메인페이지 */}
         <Link href="/">
           <div className={style.navPill}>
-            {segment && (['search', 'searchResult', 'post', 'notice', 'likes', `${me?.user?.email}`].includes(segment)) ? // segment === ''이 안 되기 때문에 include 사용
+            {segment && (['search', 'searchResult', 'post', 'notice', 'likes', 'myPage'].includes(segment)) ? // segment === ''이 안 되기 때문에 include 사용
               <>
                 <svg width={26} viewBox="0 0 24 24" aria-hidden="true"
                     className="r-18jsvk2 r-4qtqp9 r-yyyyoo r-lwhw9o r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-cnnz9e">
@@ -99,7 +99,7 @@ export default function NavMenu() {
       <li> {/*  프로필페이지 */}
         <Link href={"/myPage"}>
           <div className={style.navPill}>
-            {segment === me?.user?.email?
+            {segment === 'myPage'?
             <svg viewBox="0 0 24 24" aria-hidden="true" className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-18jsvk2 r-lwhw9o r-cnnz9e" width={26} height={26}>
               <g>
                 <path d="M17.863 13.44c1.477 1.58 2.366 3.8 2.632 6.46l.11 1.1H3.395l.11-1.1c.266-2.66 1.155-4.88 2.632-6.46C7.627 11.85 9.648 11 12 11s4.373.85 5.863 2.44zM12 2C9.791 2 8 3.79 8 6s1.791 4 4 4 4-1.79 4-4-1.791-4-4-4z"></path>
--- front/src/app/(main)/myPage/_lib/getUser.ts
+++ front/src/app/(main)/myPage/_lib/getUser.ts
@@ -4,9 +4,9 @@ import { User } from "@/model/User";
 export const getUser: QueryFunction<User, [_1: string, _2: string]>
   = async ({queryKey}) => {
     const [_1, username] = queryKey;
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users/:userId`, {
+    const res = await fetch(`/myPage`, {
       next: {
-        tags: ['users', username],
+        tags: ['myPage', username],
       },
       cache: 'no-store',
     });
