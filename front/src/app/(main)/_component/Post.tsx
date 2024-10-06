d31e02305254c51fc2d163e8181f7d10f9c4e6d6
DESKTOP-GT9KPSK\user <san9901ho@gmail.com>
2024-10-06 14:37:09 +0900

    [style] 하트 svg 테두리 추가

--- front/src/app/(main)/_component/Post.tsx
+++ front/src/app/(main)/_component/Post.tsx
@@ -26,22 +26,23 @@ export default function Post({ post }: Props) {
 
   // const onClickHeart = () => {}
   const onClickHeart = async () => {
-    const apiUrl = `/api/posts/${target.id}/like`;
+    console.log("heart");
+    // const apiUrl = `/api/posts/${target.id}/like`;
   
-    const response = await fetch(apiUrl, {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      // body: JSON.stringify({ userId: '현재 사용자 ID' }), 
-      body: JSON.stringify(`${target.userName}`), 
-    });
+    // const response = await fetch(apiUrl, {
+    //   method: 'POST', 
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   // body: JSON.stringify({ userId: '현재 사용자 ID' }), 
+    //   body: JSON.stringify(`${target.userName}`), 
+    // });
 
-    if (response.ok) {
-      setLiked(!liked); 
-    } else {
-      console.error('Failed to fetch data');
-    }
+    // if (response.ok) {
+    //   setLiked(!liked); 
+    // } else {
+    //   console.error('Failed to fetch data');
+    // }
   };
 
 
@@ -52,12 +53,6 @@ export default function Post({ post }: Props) {
         <div className={style.cover}>
           <Link href={`/${target.userName}/${target.id}`}>
             <img src={target.photoImagePath[0]} alt=""/>             
-
-            {/* 제로초 아저씨가 알려준 rewrites 써봄! */}
-            {/* <img src={`/images/${target.photoImagePath[0]}`} alt=""/>  */}
-            
-
-            {/* <img src={faker.image.urlLoremFlickr()} alt="" /> */}
           </Link>
 
           <div className={style.heartButton}>
@@ -70,7 +65,11 @@ export default function Post({ post }: Props) {
                 <svg width={30} viewBox="0 0 24 24" aria-hidden="true">
                   <g>
                     <path
-                      d="M16.697 5.5c-1.222-.06-2.679.51-3.89 2.16l-.805 1.09-.806-1.09C9.984 6.01 8.526 5.44 7.304 5.5c-1.243.07-2.349.78-2.91 1.91-.552 1.12-.633 2.78.479 4.82 1.074 1.97 3.257 4.27 7.129 6.61 3.87-2.34 6.052-4.64 7.126-6.61 1.111-2.04 1.03-3.7.477-4.82-.561-1.13-1.666-1.84-2.908-1.91zm4.187 7.69c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path>
+                      d="M16.697 5.5c-1.222-.06-2.679.51-3.89 2.16l-.805 1.09-.806-1.09C9.984 6.01 8.526 5.44 7.304 5.5c-1.243.07-2.349.78-2.91 1.91-.552 1.12-.633 2.78.479 4.82 1.074 1.97 3.257 4.27 7.129 6.61 3.87-2.34 6.052-4.64 7.126-6.61 1.111-2.04 1.03-3.7.477-4.82-.561-1.13-1.666-1.84-2.908-1.91zm4.187 7.69c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"
+                      fill="white"  // 채우기 색상을 하얀색으로 설정
+                      stroke="black" // 테두리 색상을 검은색으로 설정
+                      stroke-width="0.2" // 테두리 두께를 1로 설정
+                      ></path>
                   </g>
                 </svg>
               }
